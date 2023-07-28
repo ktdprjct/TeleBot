28ccb9350d1ff055011426296e77952e5c35d572
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-07-28 12:49:44 +0800

    update cmd/yta.js

--- cmd/yta.js
+++ cmd/yta.js
@@ -1,4 +1,4 @@
-const { youtubedl, youtubedlv2,  youtubedlv3 } = require("@bochilteam/scraper")
+let { youtubedlv2, youtubedlv3 } = require("@bochilteam/scraper")
 
 module.exports = {
   start: async function(ctx, { text }) {
@@ -9,31 +9,21 @@ module.exports = {
     if(!args[0]) return ctx.reply("Masukkan url!")
     if(!ytIdRegex.test(args[0])) return ctx.reply("Masukkan url YouTube yang valid!")
 
-    const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
-    const limitedSize = (limit ? 2000 : limit) * 1024
-    let audio, source, res, link, lastError, isLimit
-    for(let i in _audio) {
-      try {
-        audio = _audio[i]
-        if(isNaN(audio.fileSize)) continue
-
-        isLimit = limitedSize < audio.fileSize
-        if(isLimit) continue
-
-        link = await audio.download()
-        if(link) res = await fetch(link)
-
-        isLimit = res?.headers.get("content-length") && parseInt(res.headers.get("content-length")) < limitedSize
-        if(isLimit) continue
-        if(res) source = await res.arrayBuffer()
-        if(source instanceof ArrayBuffer) break
-      } catch (e) {
-        audio = link = source = null
-        lastError = e
-      }
+    const { thumbnail, audio: _audio, title } = await youtubedlv2(args[0]).catch(async _ => await youtubedlv3(args[0]))
+    let audio, res, link
+    
+    for (let i in _audio) {
+        try {
+            audio = _audio[i]
+            if (isNaN(audio.fileSize)) continue
+            link = await audio.download()
+            if (link) res = await fetch(link)
+            break
+        } catch (e) {
+            console.error(e)
+        }
     }
-    if((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) return await ctx.reply("Error: " + (lastError || "Can\"t download audio"))
-
+    
     ctx.replyWithPhoto(thumbnail, { caption: `Ukuran file: ${audio.fileSizeH}\nJudul: ${title}\n\nSedang mengirim audio...` }).then(function(msg) {
       ctx.replyWithAudio(link, { filename: title + ".mp3" }, { reply_to_message_id: msg.message_id })
     })
